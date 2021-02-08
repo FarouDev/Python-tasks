3bd91ca49b26e53c48b7158033af1e25298302df
FarouDev <76971709+FarouDev@users.noreply.github.com>
2021-02-08 23:40:47 +0200

    c

---
+++ Task 1/operators.py
@@ -0,0 +1,8 @@
+# write s simple ternary operator
+
+number_1 = input("Please enter a number: ")
+number_2 = input("please enter a second number: ")
+if(number_1 > number_2):
+    print("first number is bigger than second number" )
+else:
+    print("second number is bigger than first number")
